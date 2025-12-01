1e700057bd85008af714bda24601d021c42062a9
Yusuf Abdulrasheed <yusufabdulrasheed200@gmail.com>
2025-12-01 17:35:38 +0100

    updated the auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,71 +1,47 @@
-import Image from "next/image";
 import Link from "next/link";
-import { auth } from "@/lib/better-auth/auth";
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
+import Image from "next/image";
+import {auth} from "@/lib/better-auth/auth";
+import {headers} from "next/headers";
+import {redirect} from "next/navigation";
 
-const layout = async ({ children }: { children: React.ReactNode }) => {
-  const session = await auth.api.getSession({ headers: await headers() });
+const Layout = async ({ children }: { children : React.ReactNode }) => {
+    const session = await auth.api.getSession({ headers: await headers() })
 
-  if (session?.user) redirect("/");
-  return (
-    <main className="auth-layout">
-      <section className="auth-left-section scrollbar-hide-default">
-        <Link href="/" className="flex items-center">
-          <Image
-            src="/assets/icons/logo.svg"
-            alt="Trade Connect"
-            width={140}
-            height={32}
-            className="h-8 w-auto cursor-pointer"
-          />
-        </Link>
+    if(session?.user) redirect('/')
 
-        <div className="pb-6 lg:pb-8 flex-1">{children}</div>
-      </section>
+    return (
+        <main className="auth-layout">
+            <section className="auth-left-section scrollbar-hide-default">
+                <Link href="/" className="auth-logo">
+                    <Image src="/assets/icons/logo.svg" alt="Signalist logo" width={140} height={32} className='h-8 w-auto' />
+                </Link>
 
-      <section className="auth-right-section">
-        <div className="z-10 relative lg:mt-4 lg:mb-16">
-          <blockquote className="auth-blockqoute">
-            With Trade Connect, my watchlist finally started working for me. The
-            alerts are accurate and timely, and they’ve boosted my confidence in
-            every trade I make.
-          </blockquote>
-          <div className="flex items-center justify-between">
-            <div>
-              <cite className="auth-testimonial-author">
-                - Abdulrasheed Yusuf
-              </cite>
-              <p className="max-md:text-xs text-gray-500">Retail Investor</p>
-            </div>
+                <div className="pb-6 lg:pb-8 flex-1">{children}</div>
+            </section>
 
-            <div className="flex items-center gap-0.5">
-              {[1, 2, 3, 4, 5].map((star) => (
-                <Image
-                  src="/assets/icons/star.svg"
-                  alt="Star"
-                  key={star}
-                  width={20}
-                  height={20}
-                  className="w-5 h-5"
-                />
-              ))}
-            </div>
-          </div>
-        </div>
+            <section className="auth-right-section">
+                <div className="z-10 relative lg:mt-4 lg:mb-16">
+                    <blockquote className="auth-blockquote">
+                        Signalist turned my watchlist into a winning list. The alerts are spot-on, and I feel more confident making moves in the market
+                    </blockquote>
+                    <div className="flex items-center justify-between">
+                        <div>
+                            <cite className="auth-testimonial-author">- Abdulrasheed Yusuf.</cite>
+                            <p className="max-md:text-xs text-gray-500">Retail Investor</p>
+                        </div>
+                        <div className="flex items-center gap-0.5">
+                            {[1, 2, 3, 4, 5].map((star) => (
+                                <Image src="/assets/icons/star.svg" alt="Star" key={star} width={20} height={20} className="w-5 h-5" />
+                            ))}
+                        </div>
+                    </div>
+                </div>
 
-        <div className="flex-1 relative">
-          <Image
-            src="/assets/images/dashboard.png"
-            alt="dashboard preview"
-            width={1440}
-            height={1150}
-            className="auth-dashboard-preview absolute top-0"
-          />
-        </div>
-      </section>
-    </main>
-  );
-};
-
-export default layout;
+                <div className="flex-1 relative">
+                    <Image src="/assets/images/dashboard.png" alt="Dashboard Preview" width={1440} height={1150} className="auth-dashboard-preview absolute top-0" />
+                </div>
+            </section>
+        </main>
+    )
+}
+export default Layout
